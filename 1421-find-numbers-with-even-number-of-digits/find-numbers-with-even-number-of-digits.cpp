269e104847d24ef67e0e1880cd51441c025f040b
Shanul Haque <shanulhaque.py@gmail.com>
2026-08-28 00:08:24 +0530

    Update find-numbers-with-even-number-of-digits.cpp

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int findNumbers(vector<int>& nums) {
         int count = 0;
-/*CONVERTING TO STRING--
+/*
         for(int i=0; i<nums.size(); i++){
             string s = to_string(nums[i]);
             if( s.length() %2==0){
