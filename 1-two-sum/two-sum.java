2185a48e53ee25e475022546996e5baf80ddbfb4
Shanul Haque <shanulhaque.py@gmail.com>
2026-08-30 12:54:00 +0530

    Update two-sum.java

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum_usingHashMap(int[] nums, int target) {
         HashMap<Integer, Integer> numMap = new HashMap<>();
         for(int i=0; i<nums.length; i++){
             int comp = target - nums[i];
