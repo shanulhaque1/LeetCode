4f9d32abe9c59a9481f06fce5099e144ceb87e8a
Shanul Haque <shanulhaque.py@gmail.com>
2026-08-31 19:45:18 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (95.25%) - LeetSync

--- 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.java
+++ 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.java
@@ -1,13 +1,11 @@
 class Solution {
     public int smallestEqual(int[] nums) {
-        int result =-1;
         for(int i = 0; i<nums.length; i++){
-            if(i%10==nums[i]){
-                result =i;
-                break;}
-
+            if(i%10==nums[i])
+               return i;
+        
         }
-        return result ;
+        return -1; 
         
     }
 }
