b26a565feec430667f683f1bfd8c724a780dc7f9
Shanul Haque <shanulhaque.py@gmail.com>
2026-09-01 22:16:32 +0530

    Time: 91 ms (6.62%) | Memory: 45.4 MB (51.14%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -3,7 +3,7 @@ class Solution {
         
         for(int i=0; i<nums.length; i++){
             int count = 1;
-            int temp = nums[i];
+           
             for(int j=0; j<nums.length; j++ ){
                 if (i==j)
                 continue;
