d4e2f2bdd65f8d89e5e5af64e28f46db211fa9f0
Shanul Haque <shanulhaque.py@gmail.com>
2026-09-01 22:00:50 +0530

    Time: 91 ms (6.62%) | Memory: 45.2 MB (75.54%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        for(int i=0; i<nums.length; i++){
+            int count = 1;
+            int temp = nums[i];
+            for(int j=0; j<nums.length; j++ ){
+                if (i==j)
+                continue;
+
+                else if(nums[i]==nums[j])
+                count++;
+            }   
+            if(count!=1)
+            continue;
+
+            else
+            return nums[i];
+
+        }
+        return 0;
+
+    }
+}
