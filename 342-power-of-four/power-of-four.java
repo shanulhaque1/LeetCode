88e5772d7921a37bcaad26c982a906e6c4b486ba
Shanul Haque <shanulhaque.py@gmail.com>
2026-09-05 23:48:41 +0530

    Time: 1 ms (90.14%) | Memory: 42.7 MB (15.21%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,11 +1,14 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        int [] expo = new int[17];
+        int [] expo = new int[16];
         expo[0]= 1;
-        for(int i=1; i<17; i++){
+        if(n==expo[0])
+        return true; 
+        
+        for(int i=1; i<16; i++){
             expo[i] = expo[i-1]*4;
 
-            if(n==expo[i-1])
+            if(n==expo[i])
             return true;
 
         }
