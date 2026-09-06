203ed39012b70c5168296664f0a448cf5b8c960e
Shanul Haque <shanulhaque.py@gmail.com>
2026-09-06 17:09:47 +0530

    Time: 45 ms (55.47%) | Memory: 90.8 MB (60.64%) - LeetSync

--- 164-maximum-gap/maximum-gap.java
+++ 164-maximum-gap/maximum-gap.java
@@ -1,17 +1,16 @@
 class Solution {
     public int maximumGap(int[] nums) {
-        Arrays.sort(nums);
 
         if(nums.length <2 )
         return 0;
 
+        Arrays.sort(nums);
         int diff =0;
+
         for(int i=1; i<nums.length; i++){
             if(nums[i] - nums[i-1] > diff){
                 diff = nums[i]-nums[i-1];
             }
-
-
         }
         return diff;
     }
