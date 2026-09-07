890252cd03fa0701cada4b1a7926d19027924013
Shanul Haque <shanulhaque.py@gmail.com>
2026-09-07 15:57:03 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (85.48%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int fib(int n) {
+        int [] f = new int[n+1];
+
+        f[0]=0;
+        
+
+        if (n==0)
+        return f[0];
+
+
+        else if (n==1){
+        f[1]=1;
+        return f[1];}
+
+        else if (n>=2){
+        f[1]=1;
+        for(int i=2; i<=n; i++){
+            f[i]=f[i-1] + f[i-2];
+
+        }
+
+        }
+
+        return f[n];
+    }
+}
