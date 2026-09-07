d16bae9792cb30ed46262a449459fd3d3220ca7f
Shanul Haque <shanulhaque.py@gmail.com>
2026-09-07 16:45:48 +0530

    Time: 1 ms (29.13%) | Memory: 45.3 MB (56.68%) - LeetSync

--- 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
@@ -3,7 +3,7 @@ class Solution {
         int sign = 1;
         for(int i=0; i<nums.length; i++){
             if(nums[i]==0)
-            sign = 0;
+            return 0;
             
 
             else if(nums[i]<0)
