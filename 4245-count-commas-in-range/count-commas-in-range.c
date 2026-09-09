34133ea099cb3c9fb4ec0601d42929478f522833
Shanul Haque <shanulhaque.py@gmail.com>
2026-09-09 19:09:19 +0530

    Time: 5 ms (15.19%) | Memory: 9.3 MB (30.38%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.c
+++ 4245-count-commas-in-range/count-commas-in-range.c
@@ -4,7 +4,7 @@ int countCommas(int n) {
     int count =0;
     if(n>=1000 && n<=100000){
         for(int i=1000; i<=n;i++){
-            count ++;
+            count  ++; 
         }
     }
     return count;;
