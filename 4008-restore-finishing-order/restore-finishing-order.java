81eaed432684f35d545ba000db4fd6ac77c13ab0
Shanul Haque <shanulhaque.py@gmail.com>
2026-09-16 18:03:43 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (63.61%) - LeetSync

---
+++ 4008-restore-finishing-order/restore-finishing-order.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] recoverOrder(int[] order, int[] friends) {
+        int result [] = new int[friends.length];
+        int it=0;
+        for(int i=0 ; i<order.length; i++){
+            for(int j=0; j<friends.length; j++){
+                if(order[i]==friends[j]){
+                    result[it]=order[i];
+                    it++;
+                    
+                    
+                }
+                
+            }
+        }
+        return result;
+    }
+}
