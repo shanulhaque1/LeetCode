363235eb8399ce265ea7f2ee006f87f424e1095c
Shanul Haque <shanulhaque.py@gmail.com>
2026-09-26 16:46:09 +0530

    Time: 3 ms (100.00%) | Memory: 175.3 MB (75.58%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,36 +1,19 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int [] pos = new int [nums.length/2];
-        int p=0;
-        int [] neg = new int [nums.length/2];
-        int n=0;
-        for(int num : nums){
-            if (num>0){
-                pos[p]=num;
-                p++;
-            }
-            else
-            {
-                neg[n]=num;
-                n++;
-            }
-        }
-        int [] result= new int [nums.length];
-        int ind=0;
-        int indd=0;
+        int pi =0;
+        int ni =1;
+        int [] res = new int[nums.length];
+
         for(int i=0; i<nums.length; i++){
-            
-            if(i%2==0){
-                
-                result[i]=pos[ind];
-                ind++;
-            }
-            else
-            { 
-            result[i]=neg[indd];
-            indd++;
+            if(nums[i]> 0){
+            res[pi]=nums[i];
+            pi+=2;}
+
+            else{
+                res[ni]=nums[i];
+                ni+=2;
             }
         }
-        return result;
+        return res;
     }
 }
